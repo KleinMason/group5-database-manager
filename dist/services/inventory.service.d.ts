b5028f882151bf097951c7f52b7066ac180bbdc1
Mason Kleinsorge <Kleinsorgem97@outlook.com>
2022-03-23 11:28:58 -0500

    move to Library

--- dist/services/inventory.service.d.ts
+++
@@ -1,8 +0,0 @@
-export interface IInventoryService {
-    generateInsertStatements(productId: number, distributionCenterId: number, sizes: Array<string>, colors: Array<string>, gender: string): Promise<void>;
-}
-export declare class InventoryService implements IInventoryService {
-    constructor();
-    private randomService;
-    generateInsertStatements: (productId: number, distributionCenterId: number, sizes?: string[], colors?: string[], gender?: string) => Promise<void>;
-}
--- dist/services/random.service.d.ts
+++
@@ -1,6 +0,0 @@
-export interface IRandomService {
-    getRandomInt(max: number): any;
-}
-export declare class RandomService implements IRandomService {
-    getRandomInt: (max: number) => number;
-}
